10ac6d5131edade533554059a5aee34d89971600
RFozzy688 <pokerfozzy@gmail.com>
2024-03-24 12:50:15 +0200

    Обмен данными между компонентами. От родителя к ребенку. От ребенка к родителю

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import './styles/App.css';
 import PostList from './components/PostList';
-import MyButton from './components/UI/button/MyButton';
-import MyInput from './components/UI/input/MyInput';
+import PostForm from './components/PostForm';
+
 
 function App() {
   const [posts, setPosts] = useState([
@@ -13,35 +13,13 @@ function App() {
     {id: 5, title: 'Javascript', body: 'Description'},
   ]);
 
-  const [post, setPost] = useState({title: '', body: ''});
-
-  const addNewPost = (e) => {
-    e.preventDefault();
-
-    const newId = posts[posts.length - 1].id + 1;
-
-    setPosts([...posts, {...post, id: newId}]);
-    setPost({title: '', body: ''});
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
   }
 
   return (
     <div className='App'>
-      <form>
-        {/* Управляемый компонент */}
-        <MyInput 
-          value={post.title}
-          onChange={e => setPost({...post, title: e.target.value})}
-          type='text' 
-          placeholder='Название поста'/>
-
-        <MyInput
-          value={post.body}
-          onChange={e => setPost({...post, body: e.target.value})}
-          type='text'
-          placeholder='Описание поста' />
-
-        <MyButton onClick={addNewPost}>Добавить пост</MyButton>
-      </form>
+      <PostForm create={createPost} posts={posts}/>
       <PostList posts={posts} title={'List 1'}/>
     </div>
   );
---
+++ src/components/PostForm.jsx
@@ -0,0 +1,40 @@
+import MyButton from './UI/button/MyButton';
+import MyInput from './UI/input/MyInput';
+import React, { useState } from 'react';
+
+function PostForm({posts, create}) {
+  const [post, setPost] = useState({title: '', body: ''});
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+
+    const newId = posts[posts.length - 1].id + 1;
+    const newPost = {
+      ...post, 
+      id: newId
+    }
+    create(newPost);
+    setPost({title: '', body: ''});
+  }
+
+  return ( 
+    <form>
+      {/* Управляемый компонент */}
+      <MyInput 
+          value={post.title}
+          onChange={e => setPost({...post, title: e.target.value})}
+          type='text' 
+          placeholder='Название поста'/>
+
+        <MyInput
+          value={post.body}
+          onChange={e => setPost({...post, body: e.target.value})}
+          type='text'
+          placeholder='Описание поста' />
+
+        <MyButton onClick={addNewPost}>Добавить пост</MyButton>
+    </form>
+   );
+}
+
+export default PostForm;
